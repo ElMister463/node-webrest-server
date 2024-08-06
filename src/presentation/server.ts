98cbc43624f3e332b85c50b596a5aeaec6ba63a6
1601interfaz <usuarios1401@gmail.com>
2024-08-05 19:51:45 -0600

    Fin de la seccion 13

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,9 @@
-
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number,
+    routes: Router;
     publicPath?: string,
 }
 
@@ -11,11 +11,13 @@ export class Server {
 
     private app = express();
     private readonly port:number;
+    private readonly routes: Router;
     private readonly publicPath:string;
 
     constructor(options: Options){
-        const {port, publicPath = 'public'} = options;
+        const {port, routes,  publicPath = 'public'} = options;
         this.port = port;
+        this.routes= routes;
         this.publicPath = publicPath;
 
     }
@@ -24,11 +26,20 @@ export class Server {
     async start() {
 
 
-        //Middlewares
+        //*Middlewares
+        this.app.use( express.json() );
+        this.app.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded
 
-        //Public Folder
+
+        //*Public Folder
         this.app.use( express.static(this.publicPath));
 
+
+        //* ROUTES
+        this.app.use( this.routes );
+        
+
+        //* SPA
         this.app.get('*', (req, res) => {
             const indexPath = path.join(
               __dirname + `../../../${this.publicPath}/index.html`
